4863e52c212f8d59f17a340be58294a1fdbbec7d
Ellis Pritchard <ellis.pritchard@ft.com>
2017-05-02 17:19:48 +0100

    fix dodgy indentation

--- lib/filter_plug.ex
+++ lib/filter_plug.ex
@@ -1,39 +1,39 @@
-  defmodule Tapper.Plug.PrefixFilter do
-    @moduledoc """
-    Tapper filter plug, to prevent tracing of certain URLs.
+defmodule Tapper.Plug.PrefixFilter do
+  @moduledoc """
+  Tapper filter plug, to prevent tracing of certain URLs.
 
-    ```
-    plug Tapper.Plug.Filter, prefixes: ["__gtg", "/a/path, ["b","path"]]
-    ```
+  ```
+  plug Tapper.Plug.Filter, prefixes: ["__gtg", "/a/path, ["b","path"]]
+  ```
 
-       * `prefixes` - a list of matching path prefixes, given either as path strings, or as a list of path segment strings.
+      * `prefixes` - a list of matching path prefixes, given either as path strings, or as a list of path segment strings.
 
-    Uses `Tapper.Plug.store/2` to set state to `:ignore`.
-    """
+  Uses `Tapper.Plug.store/2` to set state to `:ignore`.
+  """
 
-    def init(opts) do
-      case Keyword.get(opts, :prefixes, []) do
-        prefixes when is_list(prefixes) -> Enum.map(prefixes, &split/1)
-        _ -> raise ArgumentError, "prefixes must be list of paths or path components"
-      end
+  def init(opts) do
+    case Keyword.get(opts, :prefixes, []) do
+      prefixes when is_list(prefixes) -> Enum.map(prefixes, &split/1)
+      _ -> raise ArgumentError, "prefixes must be list of paths or path components"
     end
-
-    def split(prefix) when is_list(prefix), do: prefix
-    def split(prefix) when is_binary(prefix) do
-      String.split(prefix, "/", trim: true)
-    end
-    def split(prefix), do: raise ArgumentError, ~S(prefixes must be a path "/a/b" or list of path components ["a","b"])
-
-    def call(conn, []), do: conn
-    def call(conn, prefixes) do
-      case is_prefix(prefixes, conn.path_info) do
-        true -> Tapper.Plug.store(conn, :ignore)
-        false -> conn
-      end
-    end
-
-    def is_prefix(prefixes, path_info) do
-      Enum.any?(prefixes, fn(prefix) -> :lists.prefix(prefix, path_info) end)
-    end
-
   end
+
+  def split(prefix) when is_list(prefix), do: prefix
+  def split(prefix) when is_binary(prefix) do
+    String.split(prefix, "/", trim: true)
+  end
+  def split(prefix), do: raise ArgumentError, ~S(prefixes must be a path "/a/b" or list of path components ["a","b"])
+
+  def call(conn, []), do: conn
+  def call(conn, prefixes) do
+    case is_prefix(prefixes, conn.path_info) do
+      true -> Tapper.Plug.store(conn, :ignore)
+      false -> conn
+    end
+  end
+
+  def is_prefix(prefixes, path_info) do
+    Enum.any?(prefixes, fn(prefix) -> :lists.prefix(prefix, path_info) end)
+  end
+
+end
