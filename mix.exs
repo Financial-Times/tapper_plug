fd3f9002eada400ef5ddcaf9bd98697ad7b9bf19
Ellis Pritchard <ellis.pritchard@ft.com>
2017-05-18 16:43:07 +0100

    fix dialyzer configuration

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule TapperPlug.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     dialyzer: [plt_add_deps: :project],
      name: "Tapper Plug",
      source_url: "https://github.com/Financial-Times/tapper_plug",
      description: "Plug integration for Tapper.",
@@ -16,7 +17,7 @@ defmodule TapperPlug.Mixfile do
   end
 
   def application do
-    [extra_applications: [:logger]]
+    [applications: [:tapper], extra_applications: [:logger]]
   end
 
   def package do
